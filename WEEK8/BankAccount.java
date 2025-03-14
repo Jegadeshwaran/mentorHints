6c40c0f4d6b83374a012074ae5ae18b99f3ed71a
Jegadeshwaran <jegadesh216@gmail.com>
2025-03-14 13:26:31 +0530

    Thread synchronization

--- WEEK8/BankAccount.java
+++ WEEK8/BankAccount.java
@@ -2,45 +2,55 @@ package WEEK8;
 
 
 class Account{
-	int balance  = 500;
-	public  void withdraw(int amt) {
-		
+	int balance  = 0;
+	
+	public synchronized void deposit(int amt) {
+		System.out.println("running 1");
+		balance = amt;
+		System.out.println("Amount successfully deposited " + balance);
+	
+}
+	
+	public  synchronized void withdraw(int amt) {
+		System.out.println("running 0");
+		 if(balance>=amt) {
 			balance = balance-amt;
-			System.out.println("balance after withdraw" + balance);
-		
+			System.out.println("balance after withdraw " + balance);
+		 }
+		 else {
+			 System.out.println("insuffient balance");
+		 }
 	}
 }
 
 class raju extends Thread{
-	Account account;
+	private Account acc;
+	private String process;
 	private int amt;
-	raju(Account acc,int amt){
-		account = acc;
+	
+	raju(Account acc,String process,int amt){
+		this.acc = acc;
+		this.process =process;
 		this.amt = amt;
-	}
+		}
+	
 	public void run() {
-		account.withdraw(amt);
-		System.out.println("raju");
-	}
-}
-class rani extends Thread{
-	Account account;
-	private int amt;
-	rani(Account acc,int amt){
-		account = acc;
-		this.amt = amt;
-	}
-	public void run() {
-		account.withdraw(amt);
-		System.out.println("rani");
+		if(process.equals("withdraw")) {
+			acc.withdraw(amt);
+		}
+		if(process.equals("deposit")) {
+			acc.deposit(amt);
+		}
+		
 	}
 }
 public class BankAccount {
 
 	public static void main(String[] args) {
 		Account account = new Account();
-		Thread t1 = new Thread(new raju(account,300));
-		Thread t2 = new Thread(new raju(account,300));
+		Thread t2 = new Thread(new raju(account,"withdraw",500));
+		Thread t1 = new Thread(new raju(account,"deposit",500));
+		
 		t1.start();
 		t2.start();
 	}
